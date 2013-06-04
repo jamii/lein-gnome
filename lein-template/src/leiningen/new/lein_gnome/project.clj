33ab0bef69832f7f2da14cdb7a80788185992f63
Jamie Brandon <jamie@scattered-thoughts.net>
2013-06-04 21:03:05 +0100

    Update versions of clojure and gnome-shell in template

--- lein-template/src/leiningen/new/lein_gnome/project.clj
+++ lein-template/src/leiningen/new/lein_gnome/project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :shell-version ["3.2.2.1"]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :shell-version ["3.8.2"]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :plugins [[lein-gnome "0.1.0-SNAPSHOT"]])
