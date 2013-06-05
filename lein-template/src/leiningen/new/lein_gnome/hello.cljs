877b21d0d2fb4f0f694a3f50801479900de91863
Jamie Brandon <jamie@scattered-thoughts.net>
2013-06-05 16:30:12 -0400

    Update hello world extension to work with gnome-shell 3.8.2

--- lein-template/src/leiningen/new/lein_gnome/hello.cljs
+++ lein-template/src/leiningen/new/lein_gnome/hello.cljs
@@ -2,47 +2,49 @@
 
 (def main js/imports.ui.main)
 
-(def text (js/imports.gi.St.Label. (.-strobj {"style_class" "helloworld-label"
-                                              "text" "Hello, ClojureScript"})))
+(def text (js/imports.gi.St.Label. (js-obj "style_class" "helloworld-label"
+                                           "text" "Hello, ClojureScript!")))
 
-(def button (js/imports.gi.St.Bin. (.-strobj {"style_class" "panel-button"
-                                              "reactive" true
-                                              "can_focus" true
-                                              "x_fill" true
-                                              "y_fill" false
-                                              "track_hover" true})))
+(def button (js/imports.gi.St.Bin. (js-obj "style_class" "panel-button"
+                                           "reactive" true
+                                           "can_focus" true
+                                           "x_fill" true
+                                           "y_fill" false
+                                           "track_hover" true)))
+
+(def icon (js/imports.gi.St.Icon. (js-obj "icon_name" "system-run-symbolic"
+                                          "style_class" "system-status-icon")))
 
 (defn hide-hello []
-  (-> main .-uiGroup (.remove_actor text)))
+  (.. main -uiGroup (remove_actor text)))
 
 (defn show-hello []
-  (-> main .-uiGroup (.add_actor text))
+  (.. main -uiGroup (add_actor text))
   (set! (.-opacity text) 255)
-  (let [monitor (-> main .-layoutManager .-primaryMonitor)]
+  (let [monitor (.. main -layoutManager -primaryMonitor)]
     (.set_position text
                    (Math/floor (- (/ (.-width monitor) 2)
                                   (/ (.-width text) 2)))
                    (Math/floor (- (/ (.-height monitor) 2)
                                   (/ (.-height text) 2))))
     (.addTween js/imports.ui.tweener
-               text (.-strobj {"opacity" 0
-                               "time" 2
-                               "transition" "easeOutQuad"
-                               "onComplete" hide-hello}))))
+               text (js-obj "opacity" 0
+                            "time" 2
+                            "transition" "easeOutQuad"
+                            "onComplete" hide-hello))))
 
 (defn init []
   (let [icon (js/imports.gi.St.Icon.
-              (.-strobj {"icon_name" "system-run"
-                         "icon_type" js/imports.gi.St.IconType.SYMBOLIC
-                         "style_class" "system-status-icon"}))]
+              (js-obj "icon_name" "system-run"
+                      "style_class" "system-status-icon"))]
     (.set_child button icon)
     (.connect button "button-press-event" show-hello)))
 
 (defn enable []
-  (-> main .-panel .-_rightBox (.insert_actor button 0)))
+  (.. main -panel -_rightBox (insert_child_at_index button 0)))
 
 (defn disable []
-  (-> main .-panel .-_rightBox (.remove_actor button)))
+  (.. main -panel -_rightBox (remove_child button)))
 
 (this-as self
          (set! (.-init self) (fn [] (init) nil))
