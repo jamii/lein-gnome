687b4c7ae38f8cd62b2515804134400936d02e19
Jamie Brandon <jamie@scattered-thoughts.net>
2013-06-05 16:16:40 -0400

    Use a more recent version of cljsbuild

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/technomancy/lein-gnome"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[lein-cljsbuild "0.1.9"]
+  :dependencies [[lein-cljsbuild "0.3.2"]
                  [cheshire "4.0.0"]]
   :eval-in-leiningen true)
