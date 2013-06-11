775d297ad1ea5cdbe6590f43b0031d2257a78480
Jamie Brandon <jamie@scattered-thoughts.net>
2013-06-11 09:29:08 +0100

    Missed project.clj in previous commits

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject lein-gnome "0.1.0-SNAPSHOT"
   :description "Bringing the magic of ClojureScript to the desktop."
-  :url "https://github.com/technomancy/lein-gnome"
+  :url "https://github.com/jamii/lein-gnome"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "4.0.0"]]
+  :dependencies [[cheshire "4.0.0"]
+                 [com.cemerick/piggieback "0.0.4"]
+                 [clj-http "0.7.2"]]
   :eval-in-leiningen true)
